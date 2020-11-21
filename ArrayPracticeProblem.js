90c665cba56a5a9f9c1d4a6702cfc9bda2b4abb0
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-11-21 14:51:24 +0530

    Maps and array problems solved

--- ArrayPracticeProblem.js
+++ ArrayPracticeProblem.js
@@ -1,7 +1,7 @@
 //--------------------------------------------------------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------------------------------------------------------
 //Program 1: To find second max and second min entry in array without using sort function
-var arr = [1,2,3,4,5,6,7,8,9,10];//[Math.floor(Math.random() * 100),Math.floor(Math.random() * 100),Math.floor(Math.random() * 100),
+var arr = [1000,220,30,465,5,46,74,8365,90147,1010];//[Math.floor(Math.random() * 100),Math.floor(Math.random() * 100),Math.floor(Math.random() * 100),
     // Math.floor(Math.random() * 100),Math.floor(Math.random() * 100),Math.floor(Math.random() * 100),Math.floor(Math.random() * 100),
     // Math.floor(Math.random() * 100),Math.floor(Math.random() * 100),Math.floor(Math.random() * 100)];
     var secondMin = 0;
@@ -28,3 +28,39 @@ console.log('Second Min= '+secondMin + '\tSecondMax= '+secondMax);
 var arr = [1000,220,30,465,5,46,74,8365,90147,1010];
 arr.sort(function(a,b){return a-b});
 console.log('Second Min= '+arr[1] + '\tSecondMax= '+arr[8]);
+//--------------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------------
+//Finding the prime factors of a number and storing the factors into an array
+var primeFactors = [];
+function findPrimeFactors(number){
+    let tempNum = number;
+    let arrayIndex = 0;
+    for(i=2; i <= (tempNum/2); i++)
+    {
+        let count = 0;
+        if(tempNum % i == 0)//condition to check if the number is a facotr of given number or not
+        {
+            for (j = 1; j <= Math.sqrt(i); j++) //for loop to check if the factor is prime number of not
+            {
+                rem = i % j;
+                if (i % j == 0) 
+                {
+                  count++;
+                }
+            }
+            if(count <= 1)
+            {
+                primeFactors[arrayIndex] = i;
+                arrayIndex++; 
+            }
+        }
+    }
+    console.log('Prime Factors of the '+number+'are: ');
+    for(i=0; i <= primeFactors.length; i++)
+    {
+        console.log(primeFactors[i]);
+    }
+}
+const prompt = require('prompt-sync')();
+var number = prompt("Enter the number to find its prime factors: ");
+findPrimeFactors(number);
